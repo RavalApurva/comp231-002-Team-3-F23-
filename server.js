18f65a9c417b9c6536807e5093bcbfa7abdf50fb
Munish-Bali <78945518+Munish-Bali@users.noreply.github.com>
2023-11-30 18:05:20 -0500

    ini

--- server.js
+++ server.js
@@ -41,6 +41,10 @@ app.get('/quizSpanish', (req, res) => {
     res.sendFile(path.join(intialPath, "quizSpanish.html"));
 })
 
+app.get('/quizFrench', (req, res) => {
+    res.sendFile(path.join(intialPath, "quizFrench.html"));
+})
+
 app.post('/register-user', (req, res) => {
     const { name, email, password } = req.body;
 
